dd2cf021a53ffc9c77b280abf025035459275f52
Timofei Gomziakov <tgomziakov@gmail.com>
2024-05-31 18:13:01 -0400

    first commit

---
+++ src/Components/MainDisplay.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "../stylesheet.css";
+
+export default function MainDisplay() {
+  let [queryResult, setQueryResult] = useState();
+  let [formValue, setFormValue] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    // geocoding
+    const geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${formValue}&limit=1&appid=${
+      import.meta.env.VITE_TOKEN
+    }`;
+    const geoResponse = await fetch(geocodeURL);
+    const geoJSON = await geoResponse.json();
+    console.log(geoJSON);
+
+    const geoCity = geoJSON[0]?.name;
+    const geoLat = geoJSON[0]?.lat;
+    const geoLon = geoJSON[0]?.lon;
+    console.log(geoLat + geoCity + geoLon);
+
+    // check for length of the return to debug
+
+    // weather api
+    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${geoLat}&lon=${geoLon}&units=imperial&appid=${
+      import.meta.env.VITE_TOKEN
+    }`;
+
+    const weatherResponse = await fetch(weatherURL);
+    const weatherJSON = await weatherResponse.json();
+    console.log(weatherJSON);
+
+    const weatherTemp = weatherJSON?.main?.temp;
+    const weatherHumidity = weatherJSON?.main?.humidity;
+    console.log(weatherTemp + " " + weatherHumidity);
+  }
+
+  function handleInput(event) {
+    setFormValue(event.target.value);
+  }
+
+  return (
+    <main id="mainWindow">
+      <h1>Weather App</h1>
+      <form className="locationForm" onSubmit={handleSubmit}>
+        <label htmlFor="cityInput">Enter your city:</label>
+        <input
+          type="text"
+          id="cityInput"
+          onChange={handleInput}
+          value={formValue}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div className="resultDisplay"></div>
+    </main>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import MainDisplay from "./Components/MainDisplay.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MainDisplay />
+  </React.StrictMode>
+);
